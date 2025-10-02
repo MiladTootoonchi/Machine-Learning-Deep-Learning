b3955ec0537ff0121334df9a28f32a92102d564e
MiladTootoonchi <milad.tootoonchi04@gmail.com>
2025-10-02 17:12:24 +0200

    changed the utilities file, because the first one didnt work

--- DAT300/CA2/utilities.py
+++ DAT300/CA2/utilities.py
@@ -4,6 +4,7 @@ from PIL import Image
 from sklearn.model_selection import train_test_split
 import pandas as pd 
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 def load_intel_dataset(
     root_dir="IntelImageClassification",
